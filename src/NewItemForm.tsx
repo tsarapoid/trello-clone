97a749537b8ceceda4f54749a45cf6a6d5c7bc3a
tsarapoid <nadein@gmail.com>
2020-11-01 19:42:16 +0300

    focus input

--- src/NewItemForm.tsx
+++ src/NewItemForm.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react"
+import { useFocus } from "./utils/useFocus"
 import { NewItemFormContainer, NewItemButton, NewItemInput } from "./styles"
 
 interface NewItemFormProps {
@@ -7,9 +8,12 @@ interface NewItemFormProps {
 
 export const NewItemForm = ({ onAdd }: NewItemFormProps) => {
 	const [text, setText] = useState("")
+		const inputRef = useFocus()
+	
 	return (
 		<NewItemFormContainer>
 			<NewItemInput
+				ref={inputRef}
 				value={text}
 				onChange={e => setText(e.target.value)}
 			/>
